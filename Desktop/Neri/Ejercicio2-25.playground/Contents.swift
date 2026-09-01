d1a2a9d5dde2224e54ca1a40602af593fcb0f81a
Angelica Ortiz <angelica.ortiz0606@gmail.com>
2026-09-01 13:35:44 -0600

    actividad 1 25 de gosto

---
+++ Desktop/Neri/Ejercicio2-25.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let name = "Neri"
+
+let favorieteQueto = "My favorite quote is \"The grass is always greener on the orde side.\""
+
+print(favorieteQueto)
+let favoriteQuote = "My favorite quote is \"The grass is always greener on the other side.\""
+
+let emptyString: String = ""
+
+if emptyString.isEmpty {
+    print("There´s nothing here")
+}else {
+    print("It´s as empty as I thought ")
+}
